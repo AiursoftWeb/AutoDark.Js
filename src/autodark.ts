b582709fe012ef7a76e86ddd0f39d6c9bc7974b1
AnduinXue <anduin@aiursoft.com>
2023-12-31 14:28:52 +0000

    init

---
+++ src/autodark.ts
@@ -0,0 +1,77 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+
+const initTheme = (): void => {
+  // Replace dark theme class
+  if (
+    window.matchMedia &&
+    window.matchMedia('(prefers-color-scheme: dark)').matches
+  ) {
+    switchTheme('dark')
+    const bodyElement = document.querySelector('body')
+    if (bodyElement) {
+      bodyElement.style.color = 'white'
+    }
+  } else {
+    switchTheme('light')
+    const bodyElement = document.querySelector('body')
+    if (bodyElement) {
+      bodyElement.style.color = 'black'
+    }
+  }
+}
+
+const switchTheme = (theme: string): void => {
+  const isDark = theme === 'dark'
+  const themeSettings: { [key: string]: string[] } = {
+    body: isDark ? ['bg-dark', 'bg-light'] : ['bg-light', 'bg-dark'],
+    table: isDark
+      ? ['table-dark', 'table-light']
+      : ['table-light', 'table-dark'],
+    '.navbar': isDark
+      ? ['navbar-dark', 'navbar-light']
+      : ['navbar-light', 'navbar-dark'],
+    '.fixed-top': isDark ? ['bg-dark', 'bg-light'] : ['bg-light', 'bg-dark'],
+    '.modal-content': isDark
+      ? ['bg-dark', 'bg-light']
+      : ['bg-light', 'bg-dark'],
+    '.container-fluid': isDark
+      ? ['bg-dark', 'bg-light']
+      : ['bg-light', 'bg-dark'],
+    '.list-group-item': isDark
+      ? ['bg-dark', 'bg-light']
+      : ['bg-light', 'bg-dark'],
+    '.content-wrapper': isDark
+      ? ['bg-dark', 'bg-light']
+      : ['bg-light', 'bg-dark'],
+    '.card': isDark ? ['bg-dark', 'bg-light'] : ['bg-light', 'bg-dark'],
+    '.bg-light': isDark ? ['bg-dark', 'bg-light'] : ['bg-light', 'bg-dark'],
+    '.bg-white': isDark ? ['bg-dark', 'bg-light'] : ['bg-light', 'bg-dark'],
+    '.bd-footer': isDark ? ['bg-dark', 'bg-light'] : ['bg-light', 'bg-dark'],
+    '.bg': isDark ? ['bg-dark', 'bg-light'] : ['bg-light', 'bg-dark'],
+  }
+
+  Object.keys(themeSettings).forEach((key) => {
+    const settings = themeSettings[key]
+    const elements = key.startsWith('.')
+      ? document.querySelectorAll(key)
+      : document.getElementsByTagName(key)
+    Array.from(elements).forEach((el: Element) => {
+      if (Array.isArray(settings)) {
+        el.classList.add(settings[0])
+        el.classList.remove(settings[1])
+      } else {
+        throw new Error('Invalid settings')
+      }
+    })
+  })
+}
+
+const autoTheme = (): void => {
+  initTheme()
+  window
+    .matchMedia('(prefers-color-scheme: light)')
+    .addEventListener('change', initTheme)
+  window.addEventListener('load', initTheme)
+}
+
+export { autoTheme, switchTheme }
